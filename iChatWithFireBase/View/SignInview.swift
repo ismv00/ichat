caf1be0989f486a01e3c9aedae74fc43ff14654f
ismv00 <ismv00@icloud.com>
2023-02-21 17:18:53 -0400

    feat:create navigation screens

--- iChatWithFireBase/View/SignInview.swift
+++ iChatWithFireBase/View/SignInview.swift
@@ -13,64 +13,67 @@ struct SignInView: View {
     @StateObject var viewModel = SignInViewModel()
     
     var body: some View {
-        VStack {
-            
-            //MARK: LOGO
-            Image("chat_logo")
-                .resizable()
-                .scaledToFit()
-                .padding()
-            
-            //MARK: INPUT TEXTS
-            TextField("Entre com seu e-mail", text: $viewModel.email)
-                .autocapitalization(.none)
-                .autocorrectionDisabled(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(RoundedRectangle(cornerRadius: 24.0)
-                    .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
-                )
-                .padding(.bottom, 20)
-            
-            
-            SecureField("Entre com sua senha", text: $viewModel.password)
-                .autocapitalization(.none)
-                .autocorrectionDisabled(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(RoundedRectangle(cornerRadius: 24.0)
-                    .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
-                )
-                .padding(.bottom, 30)
-            
-            
-            //MARK: BUTTONS
-            Button {
-                viewModel.signIn()
-            } label: {
-                Text("Entrar")
-                    .frame(maxWidth: .infinity)
+        NavigationView {
+            VStack {
+                //MARK: LOGO
+                Image("chat_logo")
+                    .resizable()
+                    .scaledToFit()
                     .padding()
-                    .background(Color("GreenColor"))
-                    .foregroundColor(Color.white)
+                
+                //MARK: INPUT TEXTS
+                TextField("Entre com seu e-mail", text: $viewModel.email)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled(false)
+                    .padding()
+                    .background(Color.white)
                     .cornerRadius(24.0)
+                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                        .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                    .padding(.bottom, 20)
+                
+                
+                SecureField("Entre com sua senha", text: $viewModel.password)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled(false)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(24.0)
+                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                        .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                    .padding(.bottom, 30)
+                
+                
+                //MARK: BUTTONS
+                Button {
+                    viewModel.signIn()
+                } label: {
+                    Text("Entrar")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("GreenColor"))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(24.0)
+                }
+                Divider()
+                    .padding()
+                
+                NavigationLink(destination: SignUpView()) {
+                    Text("Não tem uma conta? Clique aqui")
+                        .foregroundColor(Color.black)
+                }
+                
+                
+                
             }
-            Divider()
-                .padding()
-            
-            Button {
-                print("Clicado 2")
-            } label: {
-                Text("Não tem uma conta? Clique aqui")
-                    .foregroundColor(Color.black)
-            }
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
+            .navigationTitle("Login")
+            .navigationBarHidden(true) // Esconder o login na tela principal
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding(.horizontal, 32)
-        .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
     }
 }
 
--- iChatWithFireBase/View/SignUpView.swift
+++ iChatWithFireBase/View/SignUpView.swift
@@ -12,74 +12,76 @@ struct SignUpView: View {
     @StateObject var viewModel = SignUpViewModel()
     
     var body: some View {
-        VStack {
-            
-            //MARK: LOGO
-            Image("chat_logo")
-                .resizable()
-                .scaledToFit()
-                .padding()
-            
-            TextField("Entre com seu nome", text: $viewModel.name)
-                .autocapitalization(.none)
-                .autocorrectionDisabled(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(RoundedRectangle(cornerRadius: 24.0)
-                    .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
-                )
-                .padding(.bottom, 20)
-            //MARK: INPUT TEXTS
-            TextField("Entre com seu e-mail", text: $viewModel.email)
-                .autocapitalization(.none)
-                .autocorrectionDisabled(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(RoundedRectangle(cornerRadius: 24.0)
-                    .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
-                )
-                .padding(.bottom, 20)
-            
-            
-            SecureField("Entre com sua senha", text: $viewModel.password)
-                .autocapitalization(.none)
-                .autocorrectionDisabled(false)
-                .padding()
-                .background(Color.white)
-                .cornerRadius(24.0)
-                .overlay(RoundedRectangle(cornerRadius: 24.0)
-                    .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
-                )
-                .padding(.bottom, 30)
-            
-            
-            //MARK: BUTTONS
-            Button {
-                viewModel.signUp()
-            } label: {
-                Text("Entrar")
-                    .frame(maxWidth: .infinity)
+        NavigationView {
+            VStack {
+                
+                //MARK: LOGO
+                Image("chat_logo")
+                    .resizable()
+                    .scaledToFit()
                     .padding()
-                    .background(Color("GreenColor"))
-                    .foregroundColor(Color.white)
+                
+                TextField("Entre com seu nome", text: $viewModel.name)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled(false)
+                    .padding()
+                    .background(Color.white)
                     .cornerRadius(24.0)
+                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                        .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                    .padding(.bottom, 20)
+                //MARK: INPUT TEXTS
+                TextField("Entre com seu e-mail", text: $viewModel.email)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled(false)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(24.0)
+                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                        .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                    .padding(.bottom, 20)
+                
+                
+                SecureField("Entre com sua senha", text: $viewModel.password)
+                    .autocapitalization(.none)
+                    .autocorrectionDisabled(false)
+                    .padding()
+                    .background(Color.white)
+                    .cornerRadius(24.0)
+                    .overlay(RoundedRectangle(cornerRadius: 24.0)
+                        .strokeBorder(Color(UIColor.separator), style: StrokeStyle(lineWidth: 1.0))
+                    )
+                    .padding(.bottom, 30)
+                
+                
+                //MARK: BUTTONS
+                Button {
+                    viewModel.signUp()
+                } label: {
+                    Text("Entrar")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color("GreenColor"))
+                        .foregroundColor(Color.white)
+                        .cornerRadius(24.0)
+                }
+                Divider()
+                    .padding()
+                
+                NavigationLink(destination: SignUpView()) {
+                    Text("Já possui uma conta? Clique para logar.")
+                        .foregroundColor(Color.black)
+                }
+                
             }
-            Divider()
-                .padding()
-            
-            Button {
-                print("Clicado 2")
-            } label: {
-                Text("Não tem uma conta? Clique aqui")
-                    .foregroundColor(Color.black)
-            }
-            
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.horizontal, 32)
+            .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .padding(.horizontal, 32)
-        .background(Color.init(red: 240 / 255, green: 231 / 255, blue: 210 / 255))
     }
 }
 
