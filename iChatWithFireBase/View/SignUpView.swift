b6bdc30da3336063f95c89153b6f84be956f8ff0
ismv00 <ismv00@icloud.com>
2023-02-21 23:20:33 -0400

    feat:upload photo for storage firebase

--- iChatWithFireBase/View/SignUpView.swift
+++ iChatWithFireBase/View/SignUpView.swift
@@ -11,14 +11,37 @@ import SwiftUI
 struct SignUpView: View {
     @StateObject var viewModel = SignUpViewModel()
     
+    @State var isShowPhotoLibrary = false
+    
     var body: some View {
-            VStack {
-                
-                //MARK: LOGO
-                Image("chat_logo")
-                    .resizable()
-                    .scaledToFit()
+        VStack {
+            
+            Button {
+                isShowPhotoLibrary = true
+            } label: {
+                if viewModel.image.size.width > 0 {
+                    Image(uiImage: viewModel.image)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 130, height: 130)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color("GreenColor"),lineWidth: 4))
+                        .shadow(radius: 7)
+                } else {
+                    Text("Foto")
+                    .frame(width: 130, height: 130)
                     .padding()
+                    .background(Color("GreenColor"))
+                    .foregroundColor(Color.white)
+                    .cornerRadius(100.0)
+                }
+                
+            }
+                .padding(.bottom, 32)
+                .sheet(isPresented: $isShowPhotoLibrary) {
+                    ImagePicker(selectedImage: $viewModel.image)
+                }
+
                 
                 TextField("Entre com seu nome", text: $viewModel.name)
                     .autocapitalization(.none)
--- iChatWithFireBase/ViewModel/SignUpViewModel.swift
+++ iChatWithFireBase/ViewModel/SignUpViewModel.swift
@@ -7,12 +7,15 @@
 
 import Foundation
 import FirebaseAuth
+import FirebaseStorage
+import UIKit
 
 
 class SignUpViewModel : ObservableObject {
     var name = ""
     var email = ""
     var password = ""
+    @Published var image = UIImage()
     
     @Published var formInvalid = false
     
@@ -22,6 +25,15 @@ class SignUpViewModel : ObservableObject {
     
     func signUp() {
         print("Nome: \(name), email: \(email), password: \(password)")
+        
+        if(image.size.width <= 0) {
+            formInvalid = true
+            alertText = "Selecione uma foto"
+            return
+        }
+        
+        isLoading = true
+        
         Auth.auth().createUser(withEmail: email, password: password) { result, err in
             guard let user = result?.user, err == nil else {
                 self.formInvalid = true
@@ -33,6 +45,25 @@ class SignUpViewModel : ObservableObject {
             self.isLoading = false
             
             print("usuario criado \(user.uid)")
+            
+            self.uploadPhoto()
+        }
+    }
+    private func uploadPhoto() {
+        let filename = UUID().uuidString
+        
+        guard let data = image.jpegData(compressionQuality: 0.2) else { return }
+        
+        let newMetadata = StorageMetadata()
+        newMetadata.contentType = "image/jpeg"
+        
+        let ref = Storage.storage().reference(withPath: "/images/\(filename).jpg")
+        
+        ref.putData(data, metadata: newMetadata) { metadata, err in
+            ref.downloadURL {url, error in
+                self.isLoading = false
+                print("foto criada \(url)")
+            }
         }
     }
 }
